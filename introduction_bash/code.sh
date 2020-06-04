27c7bd74d7db8d275826997a548c79e6a1b8051a
Julien Robert <julienrob@gmail.com>
2020-06-04 16:43:49 +0200

    typo

--- introduction_bash/code.sh
+++ introduction_bash/code.sh
@@ -4,5 +4,5 @@ do
 done
 sleep 1
 su - etudiant
-PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ "
+PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
 clear
