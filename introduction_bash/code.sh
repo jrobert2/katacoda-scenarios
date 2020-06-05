621b93d025d17aa3eae47caf8ead8a9b53c86827
Julien Robert <julienrob@gmail.com>
2020-06-05 09:56:12 +0200

    unzip

--- introduction_bash/code.sh
+++ introduction_bash/code.sh
@@ -4,5 +4,6 @@ do
 done
 sleep 1
 su - etudiant
+unzip /tmp/home.zip
 PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
 clear
