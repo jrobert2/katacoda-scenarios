9c036a0bbea85f25dbb443a8613d800ced3e33b5
Julien Robert <julienrob@gmail.com>
2020-06-05 12:49:25 +0200

    fix

--- introduction_bash/code.sh
+++ introduction_bash/code.sh
@@ -4,6 +4,6 @@ do
 done
 sleep 1
 su - etudiant
-unzip /tmp/home.zip
+tar -pxjf /tmp/home.tar.bz2
 PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
 clear
