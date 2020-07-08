b8f801a74d998345977f0a87a0e2fc29be004837
cprevost <celine.prevost@univ-orleans.fr>
2020-07-08 15:33:47 +0200

    reparation de mes betises

--- 1-Introduction/introduction_bash/courseBase.sh
+++ 1-Introduction/introduction_bash/courseBase.sh
@@ -7,13 +7,3 @@ ssh root@host01 'sudo chown -R etudiant:etudiant /home/etudiant/.bashrc'
 ssh root@host01 'sudo chown -R etudiant:etudiant /home/etudiant/.profile'
 ssh root@host01 'sudo chown -R etudiant:etudiant /home/etudiant'
 
-ssh root@host01 'sudo useradd -p coursbash -s /bin/bash -G root -m -U sasha'
-ssh root@host01 'usermod -aG sudo sasha'
-ssh root@host01 'sudo cp /home/packer/.bashrc /home/sasha/'
-ssh root@host01 'sudo echo . /etc/profile >> /home/sasha/.bashrc'
-ssh root@host01 'sudo cp /home/packer/.profile /home/sasha/'
-ssh root@host01 'sudo chown -R sasha:sasha /home/sasha/.bashrc'
-ssh root@host01 'sudo chown -R sasha:sasha /home/sasha/.profile'
-ssh root@host01 'sudo chown -R sasha:sasha /home/sasha'
-ssh root@host01 'sudo tar -pxjf /tmp/home.tar.bz2'
-
