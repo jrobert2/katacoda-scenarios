5de66243ed7cef26e8840b8102266e0ebb2603e2
cprevost <celine.prevost@univ-orleans.fr>
2020-07-08 14:09:56 +0200

    tentative de changer le nom de l'utilisateur etudiant -> sasha

--- 1-Introduction/introduction_bash/code.sh
+++ 1-Introduction/introduction_bash/code.sh
@@ -1,9 +1,9 @@
-while ! id etudiant
+while ! id sasha
 do
     sleep 1
 done
 sleep 1
-su - etudiant
+su - sasha
 tar -pxjf /tmp/home.tar.bz2
 PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
 clear
---
+++ NOTES.md
@@ -0,0 +1,6 @@
+
+# COMPRESSER LES HOMES
+
+Dans chaque repertoire assets :
+
+`tar pcjf home.tar.bz2 home`
