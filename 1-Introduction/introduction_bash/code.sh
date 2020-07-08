74344c92b980703f74e799ec1252d3f5dd48e665
cprevost <celine.prevost@univ-orleans.fr>
2020-07-08 16:43:01 +0200

    3 utilisateurs

--- 1-Introduction/introduction_bash/code.sh
+++ 1-Introduction/introduction_bash/code.sh
@@ -17,6 +17,7 @@ sleep 1
 
 cd /
 tar -pxjf /tmp/home.tar.bz2
+chown -R etudiant:etudiant /home/etudiant/
 su - etudiant
 PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
 clear
--- 1-Introduction/introduction_bash/courseBase.sh
+++ 1-Introduction/introduction_bash/courseBase.sh
@@ -5,7 +5,7 @@ ssh root@host01 'sudo echo . /etc/profile >> /home/etudiant/.bashrc'
 ssh root@host01 'sudo cp /home/packer/.profile /home/etudiant/'
 ssh root@host01 'sudo chown -R etudiant:etudiant /home/etudiant/.bashrc'
 ssh root@host01 'sudo chown -R etudiant:etudiant /home/etudiant/.profile'
-ssh root@host01 'sudo chown -R etudiant:etudiant /home/etudiant'
+ssh root@host01 'sudo chown -R etudiant:etudiant /home/etudiant/'
 
 ssh root@host01 'sudo useradd -p coursbash -s /bin/bash -G root -m -U alice'
 ssh root@host01 'usermod -aG sudo alice'
