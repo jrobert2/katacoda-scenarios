3fc1c2ab517ba30d3935dfb3802ebfde74309b5c
cprevost <celine.prevost@univ-orleans.fr>
2020-07-08 18:50:03 +0200

    fix

--- 1-Introduction/introduction_bash/courseBase.sh
+++ 1-Introduction/introduction_bash/courseBase.sh
@@ -1,6 +1,6 @@
 
 # Utilisateur olive
-ssh root@host01 'sudo useradd -p coursbash -su olive-shell /bin/bash --create-home -U olive'
+ssh root@host01 'sudo useradd -su olive-shell /bin/bash --create-home -U olive'
 ssh root@host01 'usermod -aG sudo olive'
 # ssh root@host01 'sudo cp /home/packer/.bashrc /home/etudiant/'
 # ssh root@host01 'sudo echo . /etc/profile >> /home/etudiant/.bashrc'
