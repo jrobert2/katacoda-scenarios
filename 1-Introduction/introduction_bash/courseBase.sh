a7526b56f53378941394cd29bb19eee66d6e7a36
cprevost <celine.prevost@univ-orleans.fr>
2020-07-08 17:48:08 +0200

    essais de modif de groupes/mdp des utilisateurs

--- 1-Introduction/introduction_bash/courseBase.sh
+++ 1-Introduction/introduction_bash/courseBase.sh
@@ -1,9 +1,9 @@
 
-# Utilisateur olive
-ssh root@host01 'sudo useradd -p coursbash -s /bin/bash -G root -m -U olive'
+# Utilisateur olive 
+ssh root@host01 'sudo useradd -p coursbash -s /bin/bash -m -U olive'
 # Ajout de l'utilisateur dans le groupe 1A
-ssh root@host01 'usermod -aG sudo olive'
-
+ssh root@host01 'usermod -aG sudo 2A'
+ssh root@host01 'adduser --gecos "toto" olive'
 
 # ssh root@host01 'sudo cp /home/packer/.bashrc /home/etudiant/'
 # ssh root@host01 'sudo echo . /etc/profile >> /home/etudiant/.bashrc'
@@ -12,7 +12,7 @@ ssh root@host01 'usermod -aG sudo olive'
 # ssh root@host01 'sudo chown -R etudiant:etudiant /home/etudiant/.profile'
 
 
-ssh root@host01 'sudo useradd -p coursbash -s /bin/bash -G root -m -U alice'
+ssh root@host01 'sudo useradd -p coursbash -s /bin/bash -m -U alice'
 ssh root@host01 'usermod -aG sudo alice'
 # ssh root@host01 'sudo cp /home/packer/.bashrc /home/alice/'
 # ssh root@host01 'sudo echo . /etc/profile >> /home/alice/.bashrc'
@@ -22,7 +22,7 @@ ssh root@host01 'usermod -aG sudo alice'
 # ssh root@host01 'sudo chown -R alice:alice /home/alice'
 
 ssh root@host01 'sudo useradd -p coursbash -s /bin/bash -G root -m -U sasha'
-ssh root@host01 'usermod -aG sudo sasha'
+ssh root@host01 'usermod -aG sudo 1A'
 # ssh root@host01 'sudo cp /home/packer/.bashrc /home/sasha/'
 # ssh root@host01 'sudo echo . /etc/profile >> /home/sasha/.bashrc'
 # ssh root@host01 'sudo cp /home/sasha/.profile /home/sasha/'
