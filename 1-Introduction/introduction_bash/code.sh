c06b1aa3dbcb09c105145a29e1c8b87e06c7d0f7
cprevost <celine.prevost@univ-orleans.fr>
2020-07-08 16:30:45 +0200

    3 utilisateurs

--- 1-Introduction/introduction_bash/code.sh
+++ 1-Introduction/introduction_bash/code.sh
@@ -1,15 +1,11 @@
-while ! id alice
+while [[ ! id alice && ! id sasha && ! id etudiant ]]
 do
     sleep 1
 done
-
-while ! id etudiant
-do
-    sleep 1
-done
+sleep 1
 
 cd /
-tar -pxjf /tmp/essai.tar.bz2
+tar -pxjf /tmp/home.tar.bz2
 su - etudiant
 PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
 clear
