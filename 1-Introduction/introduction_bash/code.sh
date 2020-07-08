9aa811df71ce83140383902b3d8a90244bb4cb29
cprevost <celine.prevost@univ-orleans.fr>
2020-07-08 17:35:43 +0200

    utilisateur etudiant->sasha fix

--- 1-Introduction/introduction_bash/code.sh
+++ 1-Introduction/introduction_bash/code.sh
@@ -21,6 +21,6 @@ do
     chown -R $USER:$USER /home/$USER
 done
 
-su - etudiant
+su - sasha
 PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
 clear
