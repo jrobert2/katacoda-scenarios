2ec6c2381f13e646677343ed7d79c12422199bd1
cprevost <celine.prevost@univ-orleans.fr>
2020-07-09 10:53:45 +0200

    pour rendre l'executaion du code au démarrage transparente (nettoyage)

--- 1-Introduction/introduction_bash/code.sh
+++ 1-Introduction/introduction_bash/code.sh
@@ -3,21 +3,8 @@ while [ ! -f /tmp/creationdeshome ]
         sleep 1
     done
 sleep 1
-
 source /tmp/creationdeshome > /dev/null
-
-# cd / 
-# tar -pxjf /tmp//tmp/home.tar.bz2
-# rm home.tar.bz2
-# for USER in $LISTE
-# do
-    # cp /home/packer/.bashrc /home/$USER/
-    # echo . /etc/profile >> /home/$USER/.bashrc
-    # cp /home/packer/.profile /home/$USER/
-    # chown -R $USER:$USER /home/$USER
-# done
 rm /tmp/creationdeshome
-
 su - sasha
 PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
 clear
