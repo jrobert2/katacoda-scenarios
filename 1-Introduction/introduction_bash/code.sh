4d320fe13d4dd98b6687c33c64620b88869e4456
cprevost <celine.prevost@univ-orleans.fr>
2020-07-09 10:50:54 +0200

    pour rendre l'executaion du code au démarrage transparente

--- 1-Introduction/introduction_bash/code.sh
+++ 1-Introduction/introduction_bash/code.sh
@@ -16,6 +16,7 @@ source /tmp/creationdeshome > /dev/null
     # cp /home/packer/.profile /home/$USER/
     # chown -R $USER:$USER /home/$USER
 # done
+rm /tmp/creationdeshome
 
 su - sasha
 PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
