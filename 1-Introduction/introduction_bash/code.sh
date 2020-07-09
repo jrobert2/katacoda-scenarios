5cf7299d2047360ddf287e184c47f907851ddc6e
cprevost <celine.prevost@univ-orleans.fr>
2020-07-09 10:36:42 +0200

    pour rendre l'executaion du code au démarrage transparente

--- 1-Introduction/introduction_bash/code.sh
+++ 1-Introduction/introduction_bash/code.sh
@@ -1,25 +1,25 @@
-LISTE="alice sasha olive"
+# LISTE="alice sasha olive"
 
-for USER in $LISTE
-do
-    while ! id $USER
-    do
-        sleep 1
-    done
-    sleep 1
-done
-sleep 1
+# for USER in $LISTE
+# do
+    # while ! id $USER
+    # do
+        # sleep 1
+    # done
+    # sleep 1
+# done
+# sleep 1
 
-cd / 
-tar -pxjf /tmp//tmp/home.tar.bz2
-rm home.tar.bz2
-for USER in $LISTE
-do
-    cp /home/packer/.bashrc /home/$USER/
-    echo . /etc/profile >> /home/$USER/.bashrc
-    cp /home/packer/.profile /home/$USER/
-    chown -R $USER:$USER /home/$USER
-done
+# cd / 
+# tar -pxjf /tmp//tmp/home.tar.bz2
+# rm home.tar.bz2
+# for USER in $LISTE
+# do
+    # cp /home/packer/.bashrc /home/$USER/
+    # echo . /etc/profile >> /home/$USER/.bashrc
+    # cp /home/packer/.profile /home/$USER/
+    # chown -R $USER:$USER /home/$USER
+# done
 
 su - sasha
 PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
