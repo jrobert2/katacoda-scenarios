53dc5ac1274233b3011586d4d12ebccb5ef111c5
cprevost <celine.prevost@univ-orleans.fr>
2020-07-09 15:42:01 +0200

    cours 1.2 : preparation des homes

--- 1-Introduction/manipulation_fichiers/code.sh
+++ 1-Introduction/manipulation_fichiers/code.sh
@@ -1,9 +1,10 @@
-while ! id etudiant
-do
-    sleep 1
-done
+while [ ! -f /tmp/creationdeshome ]
+    do
+        sleep 1
+    done
 sleep 1
-su - etudiant
-tar -pxjf /tmp/home.tar.bz2
+source /tmp/creationdeshome > /dev/null
+rm /tmp/creationdeshome
+su - sasha
 PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
 clear
--- 1-Introduction/manipulation_fichiers/courseBase.sh
+++ 1-Introduction/manipulation_fichiers/courseBase.sh
@@ -1,9 +1,13 @@
-ssh root@host01 'sudo useradd -p coursbash -s /bin/bash -G root -m -U etudiant'
-ssh root@host01 'usermod -aG sudo etudiant'
-ssh root@host01 'sudo cp /home/packer/.bashrc /home/etudiant/'
-ssh root@host01 'sudo echo . /etc/profile >> /home/etudiant/.bashrc'
-ssh root@host01 'sudo cp /home/packer/.profile /home/etudiant/'
-ssh root@host01 'sudo chown -R etudiant:etudiant /home/etudiant/.bashrc'
-ssh root@host01 'sudo chown -R etudiant:etudiant /home/etudiant/.profile'
-ssh root@host01 'sudo chown -R etudiant:etudiant /home/etudiant'
+
+ssh root@host01 'sudo useradd -s /bin/bash --create-home -U olive'
+ssh root@host01 'usermod -aG sudo olive'
+ssh root@host01  'sudo echo -e "olive\nolive" | passwd olive'
+
+ssh root@host01 'sudo useradd -s /bin/bash -m -U willow'
+ssh root@host01 'usermod -aG sudo willow'
+ssh root@host01  'sudo echo -e "willow\nwillow" | passwd willow'
+
+ssh root@host01 'sudo useradd -p coursbash -s /bin/bash -G root -m -U sasha'
+ssh root@host01 'usermod -aG sudo 1A'
+ssh root@host01  'sudo echo -e "sasha\nsasha" | passwd sasha'
 
