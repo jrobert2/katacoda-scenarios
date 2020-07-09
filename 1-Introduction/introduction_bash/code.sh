6a4790cb98cef79390ab5508e4d8a9e3bfb42f4d
cprevost <celine.prevost@univ-orleans.fr>
2020-07-09 10:31:00 +0200

    pour rendre l'executaion du code au démarrage transparente

--- 1-Introduction/introduction_bash/code.sh
+++ 1-Introduction/introduction_bash/code.sh
@@ -10,17 +10,17 @@ do
 done
 sleep 1
 
-cd /
-tar -pxjf /tmp//tmp/home.tar.bz2 > /dev/null
-rm home.tar.bz2 > /dev/null
+cd / 
+tar -pxjf /tmp//tmp/home.tar.bz2
+rm home.tar.bz2
 for USER in $LISTE
 do
-    cp /home/packer/.bashrc /home/$USER/ > /dev/null
-    echo . /etc/profile >> /home/$USER/.bashrc > /dev/null
-    cp /home/packer/.profile /home/$USER/ > /dev/null
-    chown -R $USER:$USER /home/$USER > /dev/null
+    cp /home/packer/.bashrc /home/$USER/
+    echo . /etc/profile >> /home/$USER/.bashrc
+    cp /home/packer/.profile /home/$USER/
+    chown -R $USER:$USER /home/$USER
 done
 
-su - sasha > /dev/null
-PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ ' > /dev/null
+su - sasha
+PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
 clear
