a91de2670705aa7c02973ce1e7ab0f09ca898467
cprevost <celine.prevost@univ-orleans.fr>
2020-07-10 11:26:16 +0200

    refactor du scenario nano

--- 1-Introduction/nano/code.sh
+++ 1-Introduction/nano/code.sh
@@ -4,6 +4,7 @@ do
 done
 tar -pxjf /tmp/home.tar.bz2
 rm /tmp/home.tar.bz2
-PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
+chown -R sasha:sasha /home/sasha
 su - sasha
+PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
 clear
