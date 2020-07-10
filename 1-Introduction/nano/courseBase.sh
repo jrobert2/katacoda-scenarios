858ccd2b088f2a2a6777bf1bf3c2053d7148b4e2
cprevost <celine.prevost@univ-orleans.fr>
2020-07-10 12:30:12 +0200

    test de modif courseBase

--- 1-Introduction/nano/courseBase.sh
+++ 1-Introduction/nano/courseBase.sh
@@ -1,17 +1,21 @@
 #! /bin/bash
 
 USER=sacha
+MDP=sasha
+GROUP=sasha
+
 #création de l'utilisateur
-ssh root@host01 "sudo useradd -s /bin/bash -G root -m -U sasha"
+ssh root@host01 "sudo useradd -s /bin/bash -G root -m -U $USER"
+# Ajout au groupe
+ssh root@host01 "sudo usermod -aG $GROUP $USER"
 # attribution du mdp
-ssh root@host01  "sudo echo -e 'sasha\nsasha' | passwd sasha"
+ssh root@host01  "sudo echo -e '$MDP\n$MDP' | passwd $USER"
 # config du profile
-ssh root@host01 'sudo cp /home/packer/.bashrc /home/sasha/'
-ssh root@host01 'sudo echo . /etc/profile >> /home/sasha/.bashrc'
-ssh root@host01 'sudo cp /home/packer/.profile /home/sasha/'
+ssh root@host01 "sudo cp /home/packer/.bashrc /home/$USER/"
+ssh root@host01 "sudo echo . /etc/profile >> /home/$USER/.bashrc"
+ssh root@host01 "sudo cp /home/packer/.profile /home/$USER/"
 # transfert des droits
-ssh root@host01 'sudo chown -R sasha:sasha /home/sasha'
+ssh root@host01 "sudo chown -R $USER:$GROUP /home/$USER"
 
-sudo useradd -s /bin/bash -G root -m -U toto
-
-ssh root@host01 'echo "done" > /tmp/.fini'
+# fin du programme
+ssh root@host01 "echo 'done' > /tmp/.fini"
