7328c73dfcf3d7c93a390ba62d11444e2bfa3d1e
cprevost <celine.prevost@univ-orleans.fr>
2020-07-10 11:44:51 +0200

    test de modif courseBase

--- 1-Introduction/nano/code.sh
+++ 1-Introduction/nano/code.sh
@@ -1,10 +1,3 @@
-while [ ! id sasha ] || [ ! -f /tmp/home.tar.bz2 ]
-do
-    sleep 1
-done
-tar -pxjf /tmp/home.tar.bz2
-rm /tmp/home.tar.bz2
-chown -R sasha:sasha /home/sasha
 su - sasha
 PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
 clear
--- 1-Introduction/nano/courseBase.sh
+++ 1-Introduction/nano/courseBase.sh
@@ -7,7 +7,12 @@ ssh root@host01  'sudo echo -e "sasha\nsasha" | passwd sasha'
 ssh root@host01 'sudo cp /home/packer/.bashrc /home/sasha/'
 ssh root@host01 'sudo echo . /etc/profile >> /home/sasha/.bashrc'
 ssh root@host01 'sudo cp /home/packer/.profile /home/sasha/'
+# remplissage des homes
+ssh root@host01 'tar -pxjf /tmp/home.tar.bz2'
 # transfert des droits
 # ssh root@host01 'sudo chown -R sasha:sasha /home/sasha/.bashrc'
 # ssh root@host01 'sudo chown -R sasha:sasha /home/sasha/.profile'
-# ssh root@host01 'sudo chown -R sasha:sasha /home/sasha'
+ssh root@host01 'sudo chown -R sasha:sasha /home/sasha'
+ 
+
+sudo useradd -s /bin/bash -G root -m -U toto
