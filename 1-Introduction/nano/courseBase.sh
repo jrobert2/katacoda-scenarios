0565c6f4cfe41aa5b82865dd0d714ba151dcc458
cprevost <celine.prevost@univ-orleans.fr>
2020-07-10 11:23:50 +0200

    refactor du scenario nano

--- 1-Introduction/nano/courseBase.sh
+++ 1-Introduction/nano/courseBase.sh
@@ -1,12 +1,13 @@
-#création de l'utilisateur sasha
+USER=sacha
+#création de l'utilisateur
 ssh root@host01 'sudo useradd -s /bin/bash -G root -m -U sasha'
 # attribution du mdp
 ssh root@host01  'sudo echo -e "sasha\nsasha" | passwd sasha'
 # config du profile
-ssh root@host01 'sudo cp /home/packer/.bashrc /home/etudiant/'
-ssh root@host01 'sudo echo . /etc/profile >> /home/etudiant/.bashrc'
-ssh root@host01 'sudo cp /home/packer/.profile /home/etudiant/'
+ssh root@host01 'sudo cp /home/packer/.bashrc /home/sasha/'
+ssh root@host01 'sudo echo . /etc/profile >> /home/sasha/.bashrc'
+ssh root@host01 'sudo cp /home/packer/.profile /home/sasha/'
 # transfert des droits
-ssh root@host01 'sudo chown -R etudiant:etudiant /home/etudiant/.bashrc'
-ssh root@host01 'sudo chown -R etudiant:etudiant /home/etudiant/.profile'
-ssh root@host01 'sudo chown -R etudiant:etudiant /home/etudiant'
+ssh root@host01 'sudo chown -R sasha:sasha /home/sasha/.bashrc'
+ssh root@host01 'sudo chown -R sasha:sasha /home/sasha/.profile'
+ssh root@host01 'sudo chown -R sasha:sasha /home/sasha'
