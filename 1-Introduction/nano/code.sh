d2f29ac9bb51839c008c1283706d0c58fa8de617
cprevost <celine.prevost@univ-orleans.fr>
2020-07-10 12:37:35 +0200

    test de modif code

--- 1-Introduction/nano/code.sh
+++ 1-Introduction/nano/code.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+
 bash << 'EOF'
 
     clear
@@ -14,13 +15,14 @@ bash << 'EOF'
     cd /
     tar -pxjf /tmp/home.tar.bz2
     chown -R sasha:sasha /home/sasha
+    
     # nettoyage
     rm /tmp/.fini
     rm /tmp/home.tar.bz2
+
+EOF
 
     # Transformation en sacha
     su - sasha
     PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
     clear
-
-EOF
