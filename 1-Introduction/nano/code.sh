6552056db3f14dcd55ee2e48033e13b7d801ca31
cprevost <celine.prevost@univ-orleans.fr>
2020-07-10 12:09:09 +0200

    test de modif courseBase

--- 1-Introduction/nano/code.sh
+++ 1-Introduction/nano/code.sh
@@ -7,7 +7,9 @@ do
     echo -n "."
 done
 # remplissage des homes
+cd /
 tar -pxjf /tmp/home.tar.bz2
+chown -R sasha:sasha /home/sasha
 su - sasha
 PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
 clear
--- 1-Introduction/nano/courseBase.sh
+++ 1-Introduction/nano/courseBase.sh
@@ -8,7 +8,7 @@ ssh root@host01  'sudo echo -e "sasha\nsasha" | passwd sasha'
 # config du profile
 ssh root@host01 'sudo cp /home/packer/.bashrc /home/sasha/'
 ssh root@host01 'sudo echo . /etc/profile >> /home/sasha/.bashrc'
-#ssh root@host01 'sudo cp /home/packer/.profile /home/sasha/'
+ssh root@host01 'sudo cp /home/packer/.profile /home/sasha/'
 
 # transfert des droits
 ssh root@host01 'sudo chown -R sasha:sasha /home/sasha'
