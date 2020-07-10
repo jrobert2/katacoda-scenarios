11fc0d2e21a75a6d42073ebb411b710b50d1f19c
cprevost <celine.prevost@univ-orleans.fr>
2020-07-10 12:21:04 +0200

    test de modif courseBase

--- 1-Introduction/nano/code.sh
+++ 1-Introduction/nano/code.sh
@@ -6,12 +6,18 @@ do
     sleep 1s
     echo -n "."
 done
+
 # remplissage des homes
 cd /
 tar -pxjf /tmp/home.tar.bz2
 chown -R sasha:sasha /home/sasha
+# nettoyage
+rm /tmp/.fini
+rm /tmp/home.tar.bz2
+
+# Transformation en sacha
 su - sasha
-PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
+# PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
 clear
 
 echo "code terminé !!"
--- 1-Introduction/nano/courseBase.sh
+++ 1-Introduction/nano/courseBase.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
 
+# ajout de la coloration dans le profile par défaut (on doit pouvoir faire autrement ...)
+ssh root@host01 "sudo echo PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ ' >> /etc/profile"
+
+
 USER=sacha
 #création de l'utilisateur
-ssh root@host01 'sudo useradd -s /bin/bash -G root -m -U sasha'
+ssh root@host01 "sudo useradd -s /bin/bash -G root -m -U sasha"
 # attribution du mdp
-ssh root@host01  'sudo echo -e "sasha\nsasha" | passwd sasha'
+ssh root@host01  "sudo echo -e 'sasha\nsasha' | passwd sasha"
+
 # config du profile
 ssh root@host01 'sudo cp /home/packer/.bashrc /home/sasha/'
 ssh root@host01 'sudo echo . /etc/profile >> /home/sasha/.bashrc'
+
 ssh root@host01 'sudo cp /home/packer/.profile /home/sasha/'
 
 # transfert des droits
