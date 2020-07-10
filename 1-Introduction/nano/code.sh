c52498e46351356d21189ecdf5fe12c2e1e2f7f1
cprevost <celine.prevost@univ-orleans.fr>
2020-07-10 12:23:09 +0200

    test de modif courseBase

--- 1-Introduction/nano/code.sh
+++ 1-Introduction/nano/code.sh
@@ -17,7 +17,7 @@ rm /tmp/home.tar.bz2
 
 # Transformation en sacha
 su - sasha
-# PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
+PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
 clear
 
 echo "code terminé !!"
