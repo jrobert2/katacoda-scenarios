d99222fde1d28457e2dd9e53bda30b5bc792cca5
cprevost <celine.prevost@univ-orleans.fr>
2020-07-10 12:31:26 +0200

    test de modif courseBase

--- 1-Introduction/nano/courseBase.sh
+++ 1-Introduction/nano/courseBase.sh
@@ -7,7 +7,7 @@ GROUP=sasha
 #création de l'utilisateur
 ssh root@host01 "sudo useradd -s /bin/bash -G root -m -U $USER"
 # Ajout au groupe
-ssh root@host01 "sudo usermod -aG $GROUP $USER"
+# ssh root@host01 "sudo usermod -aG $GROUP $USER"
 # attribution du mdp
 ssh root@host01  "sudo echo -e '$MDP\n$MDP' | passwd $USER"
 # config du profile
@@ -17,5 +17,5 @@ ssh root@host01 "sudo cp /home/packer/.profile /home/$USER/"
 # transfert des droits
 ssh root@host01 "sudo chown -R $USER:$GROUP /home/$USER"
 
-# fin du programme
+# fin de la config
 ssh root@host01 "echo 'done' > /tmp/.fini"
