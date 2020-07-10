ba8cceafa9523b6b104824093b5688f3687cab48
cprevost <celine.prevost@univ-orleans.fr>
2020-07-10 12:43:28 +0200

    test de modif code

--- 1-Introduction/nano/code.sh
+++ 1-Introduction/nano/code.sh
@@ -24,6 +24,6 @@ bash << 'EOF'
 EOF
 
 # Transformation en sacha
-su - sasha
-PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
-clear
+    su - sasha
+    PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$(pwd)\[\033[00m\]\$ '
+    clear
