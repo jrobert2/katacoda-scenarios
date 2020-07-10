27fb27d66dc19af611ce6b067d42d69c374cd39d
cprevost <celine.prevost@univ-orleans.fr>
2020-07-10 12:25:25 +0200

    test de modif courseBase

--- 1-Introduction/nano/courseBase.sh
+++ 1-Introduction/nano/courseBase.sh
@@ -8,14 +8,10 @@ ssh root@host01  "sudo echo -e 'sasha\nsasha' | passwd sasha"
 # config du profile
 ssh root@host01 'sudo cp /home/packer/.bashrc /home/sasha/'
 ssh root@host01 'sudo echo . /etc/profile >> /home/sasha/.bashrc'
-
 ssh root@host01 'sudo cp /home/packer/.profile /home/sasha/'
-
 # transfert des droits
 ssh root@host01 'sudo chown -R sasha:sasha /home/sasha'
- 
-# sudo useradd -s /bin/bash -G root -m -U toto
+
+sudo useradd -s /bin/bash -G root -m -U toto
 
 ssh root@host01 'echo "done" > /tmp/.fini'
-
-ssh root@host01 'echo "courseBase terminé !!"'
