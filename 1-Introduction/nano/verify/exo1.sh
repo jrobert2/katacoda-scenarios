0ce911dceabe0bb2bf725e048d5192317ac44b6e
cprevost <celine.prevost@univ-orleans.fr>
2020-07-10 14:03:23 +0200

    debug les verif

--- 1-Introduction/nano/verify/exo1.sh
+++ 1-Introduction/nano/verify/exo1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source /tmp/.env_sasha
-if [ $(cat $HOME/Documents/fichier1.txt | grep -i "Bonjour" | wc -l) -ge 1 ]
+# source /tmp/.env_sasha
+if [ $(cat /home/sasha/Documents/fichier1.txt | grep -i "Bonjour" | wc -l) -ge 1 ]
 then
     echo done
 fi
--- 1-Introduction/nano/verify/exo2.sh
+++ 1-Introduction/nano/verify/exo2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source /tmp/.env_sasha
-if [  $(cat $HOME/Documents/fichier10.txt | grep -i "geek" | wc -l) -ge 1 ]
+#source /tmp/.env_sasha
+if [  $(cat /home/sasha/Documents/fichier10.txt | grep -i "geek" | wc -l) -ge 1 ]
 then
     echo done
 fi
