8b03d881514adcc91e8018d128ba7e10bfb532e4
cprevost <celine.prevost@univ-orleans.fr>
2020-07-16 19:02:17 +0200

    fix ?

--- 2-Decoupage-a-trier/droits_octal/verify/chmod2.sh
+++ 2-Decoupage-a-trier/droits_octal/verify/chmod2.sh
@@ -1,10 +1,7 @@
 if [ "$(ls -l /home/sasha | grep exemple2 | cut -d' ' -f1)" == "-rwxrw-r--" ];
 then
-    if [ "$(ls -l /home/sasha | grep exemple3 | cut -d' ' -f1)" == "drwx----w-" ]
+    if [ "$(ls -l /home/sasha | grep exemple3 | cut -d' ' -f1)" == "drwx----w-" ] &&  [ "$(ls -l /home/sasha | grep exemple4 | cut -d' ' -f1)" == "-rwx------" ];
     then
-        if [ "$(ls -l /home/sasha | grep exemple4 | cut -d' ' -f1)" == "-rwx------" ]
-        then
-            echo done
-        fi
+        echo done;
     fi
 fi
