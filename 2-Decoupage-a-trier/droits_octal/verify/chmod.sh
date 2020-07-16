e36d97dcccb7ddcd5a52a1019e0bf12b4c1cc17d
cprevost <celine.prevost@univ-orleans.fr>
2020-07-16 18:34:38 +0200

    fix

--- 2-Decoupage-a-trier/droits_octal/verify/chmod.sh
+++ 2-Decoupage-a-trier/droits_octal/verify/chmod.sh
@@ -1,5 +1,5 @@
 source /tmp/.env_sasha
-if [ "$(ls -l /home/sasha | grep exemple2 | cut -d" " -f1)" == "-rw-r-----" ]
+if [ "$(ls -l /home/sasha | grep exemple2 | cut -d' ' -f1)" == "-rw-r-----" ]
 then
     echo done
 fi
