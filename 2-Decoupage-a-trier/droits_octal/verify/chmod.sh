8005865e2526a76de29c9484d54eeb8a91512b4d
cprevost <celine.prevost@univ-orleans.fr>
2020-07-17 11:38:23 +0200

    fix

--- 2-Decoupage-a-trier/droits_octal/verify/chmod.sh
+++ 2-Decoupage-a-trier/droits_octal/verify/chmod.sh
@@ -1,5 +1,1 @@
-source /tmp/.env_sasha
-if [ "$(ls -l /home/sasha | grep exemple2 | cut -d' ' -f1)" == "-rw-r-----" ]
-then
-    echo "done"
-fi
+[ $(ls -l /home/sasha | grep exemple2 | cut -d" " -f1) == "-rw-r-----" ] && echo "done"
