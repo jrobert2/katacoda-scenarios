556dda22aceb78968a3003b11036cf6e1838bd61
Emmauel Melin <emmanuel.melin@univ-orleans.fr>
2020-12-02 10:00:47 +0100

    best sync

--- test/assets/courseBase.sh
+++ test/assets/courseBase.sh
@@ -9,31 +9,31 @@ ssh -o 'ConnectionAttempts 10' root@host01 "sudo useradd -s /bin/bash -G root -m
 wait
 
 # attribution du mdp
-ssh -o 'ConnectionAttempts 10' root@host01  "sudo echo -e '$MDP\n$MDP' | passwd $USER"
-wait
+ssh -o 'ConnectionAttempts 10' root@host01  "sudo echo -e '$MDP\n$MDP' | passwd $USER" &
+
 
 # config du profile
-ssh -o 'ConnectionAttempts 10' root@host01 "sudo cp /home/packer/.bashrc /home/$USER/"
-ssh -o 'ConnectionAttempts 10' root@host01 "sudo echo . /etc/profile >> /home/$USER/.bashrc"
-ssh -o 'ConnectionAttempts 10' root@host01 "sudo cp /home/packer/.profile /home/$USER/"
+ssh -o 'ConnectionAttempts 10' root@host01 "sudo cp /home/packer/.bashrc /home/$USER/" &
+ssh -o 'ConnectionAttempts 10' root@host01 "sudo echo . /etc/profile >> /home/$USER/.bashrc" &
+ssh -o 'ConnectionAttempts 10' root@host01 "sudo cp /home/packer/.profile /home/$USER/" &
 # transfert des droits
-ssh -o 'ConnectionAttempts 10' root@host01 "sudo chown -R $USER:$USER /home/$USER"
+ssh -o 'ConnectionAttempts 10' root@host01 "sudo chown -R $USER:$USER /home/$USER" &
 
 #création d'autres groupes/utilisateurs
-ssh -o 'ConnectionAttempts 10' root@host01 "sudo useradd -s /bin/bash -G root -m -U willow"
-ssh -o 'ConnectionAttempts 10' root@host01 "sudo useradd -s /bin/bash -G root -m -U ariel"
-ssh -o 'ConnectionAttempts 10' root@host01 "sudo useradd -s /bin/bash -G root -m -U olympe"
+ssh -o 'ConnectionAttempts 10' root@host01 "sudo useradd -s /bin/bash -G root -m -U willow" &
+ssh -o 'ConnectionAttempts 10' root@host01 "sudo useradd -s /bin/bash -G root -m -U ariel" &
+ssh -o 'ConnectionAttempts 10' root@host01 "sudo useradd -s /bin/bash -G root -m -U olympe" &
 
 # Ajout aux groupes
-ssh -o 'ConnectionAttempts 10' root@host01 "sudo addgroup etu"
-ssh -o 'ConnectionAttempts 10' root@host01 "sudo addgroup prof"
+ssh -o 'ConnectionAttempts 10' root@host01 "sudo addgroup etu" &
+ssh -o 'ConnectionAttempts 10' root@host01 "sudo addgroup prof" &
 
 wait
-ssh -o 'ConnectionAttempts 10' root@host01 "sudo adduser sasha etu"
-ssh -o 'ConnectionAttempts 10' root@host01 "sudo adduser willow etu"
-ssh -o 'ConnectionAttempts 10' root@host01 "sudo adduser ariel prof"
+ssh -o 'ConnectionAttempts 10' root@host01 "sudo adduser sasha etu" &
+ssh -o 'ConnectionAttempts 10' root@host01 "sudo adduser willow etu" &
+ssh -o 'ConnectionAttempts 10' root@host01 "sudo adduser ariel prof" &
 
 # fin de la config
 wait
 ssh -o 'ConnectionAttempts 10' root@host01 "echo 'done' > /tmp/.fini"
-wait
+
